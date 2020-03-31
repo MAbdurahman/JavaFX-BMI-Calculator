1ebda79390e6d0272b1a3e3d0ba6763ec56a6b3d
MAbdurahman <mdbdrrhm@gmail.com>
2020-03-31 07:36:21 -0500

    Added BMI icon image, README, and LICENSE; made some base project configurations.

---
+++ src/app/Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("bmi_calculator.fxml"));
+
+        Image icon = new Image(Main.class.getResourceAsStream("/img/body-mass-index-icon.png"));
+        primaryStage.getIcons().add(icon);
+
+        primaryStage.setTitle(" B M I Calculator");
+        primaryStage.setScene(new Scene(root, 500, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
